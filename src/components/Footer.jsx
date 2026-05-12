92a258d0b1fb2fd09c57114e486f3ef4a72391eb
EvgenKytok <kutok674@gmail.com>
2026-05-12 03:54:07 +0300

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => (
   <footer>
     <p>Звʼяжись зі мною: 
       <a href="https://github.com/EvgenKytok" target="_blank" rel="noreferrer"> GitHub</a> | 
-      <a kn24-ye.kuto@nubip.edu.ua"> Email</a>
+      <a kn24-ye.kutok@nubip.edu.ua"> Email</a>
     </p>
   </footer>
 );
